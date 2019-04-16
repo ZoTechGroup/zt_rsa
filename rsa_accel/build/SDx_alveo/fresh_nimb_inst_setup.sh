8b9ada0024a5964856ec59915fc4cf845c8b3234
Alexander Kropotov <alexander.kropotov@zotechgroup.com>
2019-04-16 16:29:06 +0900

    Update of initial setup script for fresh Nimbix application (docker container).

--- rsa_accel/build/SDx_alveo/fresh_nimb_inst_setup.sh
+++ rsa_accel/build/SDx_alveo/fresh_nimb_inst_setup.sh
@@ -1,30 +1,22 @@
 # Setup actions for newly created Nimbix instance
 
-sudo apt update
-# sudo apt-get upgrade # system update may take a long time
-sudo apt install build-essential
-sudo apt install gcc
-sudo apt install g++
-sudo apt install make
-sudo apt install git
-sudo apt install subversion
-sudo apt install mc
-sudo apt install libgmp-dev # required for modular exponentiation
-sudo apt install libpthread-stubs0-dev # for pthread lib update
+sudo apt -y update
+sudo apt -y upgrade # system update may take a long time
+sudo apt -y install build-essential
+sudo apt -y install gcc
+sudo apt -y install g++
+sudo apt -y install make
+sudo apt -y install git
+sudo apt -y install subversion
+sudo apt -y install mc
+sudo apt -y install libgmp-dev # required for modular exponentiation
+sudo apt -y install libpthread-stubs0-dev # for pthread lib update
 
 # Installing OpenSSL
-sudo apt install perl
-sudo apt install libtext-template-perl
-
-export OPENSSL_DIR=/home/nimbix/data/openssl
-if [ -d "$OPENSSL_DIR" ]; then
-	echo "$OPENSSL_DIR already exists, just installing OpenSSL:"
-	cd $OPENSSL_DIR
-	sudo make install
-else
-	echo "OPENSSL_DIR doesn't exist, downloading and installing OpenSSL:"
-    source ~/data/zt_rsa/rsa_use/install_openssl.sh
-fi
+sudo apt -y install perl
+sudo apt -y install libtext-template-perl
+export OPENSSL_DIR=/opt/example/openssl
+source /opt/example/zt_rsa/rsa_use/install_openssl.sh
 
 # System reboot
-sudo shutdown -r now
+#sudo shutdown -r now
