dcf03cae56147e4fe7af7ca82e4307d1b3aa0cdc
Alexander Kropotov <alexander.kropotov@zotechgroup.com>
2019-04-16 16:54:00 +0900

    Changing "source" "." for bash script as "source" doesn't work in dockerfile.

--- rsa_accel/build/SDx_alveo/fresh_nimb_inst_setup.sh
+++ rsa_accel/build/SDx_alveo/fresh_nimb_inst_setup.sh
@@ -1,7 +1,7 @@
 # Setup actions for newly created Nimbix instance
 
 sudo apt -y update
-sudo apt -y upgrade # system update may take a long time
+sudo apt -y upgrade
 sudo apt -y install build-essential
 sudo apt -y install gcc
 sudo apt -y install g++
@@ -16,7 +16,7 @@ sudo apt -y install libpthread-stubs0-dev # for pthread lib update
 sudo apt -y install perl
 sudo apt -y install libtext-template-perl
 export OPENSSL_DIR=/opt/example/openssl
-source /opt/example/zt_rsa/rsa_use/install_openssl.sh
+. /opt/example/zt_rsa/rsa_use/install_openssl.sh
 
 # System reboot
 #sudo shutdown -r now
