c9cbd7347614334a204ae523ae18ce2882e9db39
Alexander Kropotov <alexander.kropotov@zotechgroup.com>
2019-07-14 15:24:23 +0900

    Changing -std=c++14 option to -std=c++1y as synonym, but more familiar to compiler on Centos.

--- rsa_accel/build/SDx_aws/make_host_app.sh
+++ rsa_accel/build/SDx_aws/make_host_app.sh
@@ -5,7 +5,7 @@ if [ -z "$OPENSSL_DIR" ]; then
     exit 2
 fi
 
-# Building sw part only of AWS FPGA-accelerated application
+# Building sw part only of AWS FPGA-accelerated application (-std=c++1y is synonym to s-std=c++14)
 g++ ../../src/rsa_test.cpp\
     ../../src/rsa_gmp.cpp\
     ../../src/rsa_pow.cpp\
@@ -16,6 +16,6 @@ g++ ../../src/rsa_test.cpp\
     -I$XILINX_VIVADO/include\
     -I$OPENSSL_DIR/include\
     -DMAX_RSA_BITS=2048 -DUSE_OPENCL -DUNIT_COUNT=400\
-    -O3 -DNDEBUG -std=c++14 -pthread\
+    -O3 -DNDEBUG -std=c++1y -pthread\
     -lpthread -lgmp -lOpenCL -L$OPENSSL_DIR -lcrypto\
     -o test_rsa_hls
